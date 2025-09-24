516d0fe47faaf6c64b3eda989fc292ae2069f522
amitverma8835 <amitkv93047@gmail.com>
2025-09-24 13:33:25 +0530

    config url changed

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
   DB_URL:
     process.env.URL ||
-    "mongodb+srv://TAAZADANDIYA:ROHIT@01@cmongodb+srv://TAAZADANDIYA:ROHIT%4001@cluster0.xmofqnm.mongodb.net/taaza-dandiya?retryWrites=true&w=majority&appName=Cluster0luster0.xmofqnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://TAAZADANDIYA:ROHIT%4001@cluster0.xmofqnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   PORT: process.env.PORT || 7000,
 };
 
